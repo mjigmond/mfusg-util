74532688dcb21e464df61df23991cbbcc4faad8a
Marius Jigmond <mjigmond@gmail.com>
2019-10-26 09:50:51 -0600

    Initial commit with CBB parser

---
+++ parseCBBu.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 09:06:58 2019
+
+@author: marius
+"""
+
+import numpy as np
+import os
+
+def parseCBBu(fn=None):
+    fSize = os.path.getsize(fn)
+    print(fSize)
+    ofst = 0
+    BUD = {}
+    while ofst < fSize:
+        dtmeta = np.dtype([('kstp', 'i4'), ('kper', 'i4'), ('text', 'S16'), ('nval', 'i4'), ('one', 'i4'), ('icode', 'i4')])
+        meta = np.memmap(fn, mode='r', dtype=dtmeta, offset=ofst, shape=1)[0]
+        arrSize = meta['nval']
+        dtu = np.dtype([('kstp', 'i4'), ('kper', 'i4'), ('text', 'S16'), ('nval', 'i4'), ('one', 'i4'), ('icode', 'i4'), ('data', 'f4', arrSize)])
+        data = np.memmap(fn, mode='r', dtype=dtu, offset=ofst, shape=1)[0]
+        ofst += dtu.itemsize
+        kper = data['kper']
+        kstp = data['kstp']
+        text = data['text'].decode().strip()
+        BUD[(text, arrSize), kper, kstp] = data['data']
+    return BUD
+
+fName = 'model.cbb'
+cbb = parseCBBu(fName)
